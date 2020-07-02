553bb158b2b0c2af3b907d07e0735b906a836341
albert129github <albertwei129@gmail.com>
2020-07-02 13:42:04 +0800

    Add files via upload

---
+++ testsample_post.php
@@ -0,0 +1,10 @@
+<?php
+	$connectionInfo = array("Database"=>"test", "CharacterSet" => "UTF-8", "UID" => "test", "PWD" => "test");
+	$dblink = sqlsrv_connect("127.0.0.1", $connectionInfo);
+
+	$user = $_POST['test'];
+	$sql = "SELECT * FROM user where name = '$user' and active = 1";
+	$result = sqlsrv_query($dblink, $sql);
+	$row = sqlsrv_fetch_array($result, SQLSRV_FETCH_ASSOC);
+
+?>
